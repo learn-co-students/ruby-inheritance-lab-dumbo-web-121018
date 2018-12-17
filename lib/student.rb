784710a37b4fbd8dc9020fc5cf46bb530b858bbc
milespucarelli <miles.pucarelli@gmail.com>
2018-12-17 16:36:53 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+  attr_reader :knowledge
 
+  def initialize(first_name = "", last_name = "")
+    super(first_name, last_name)
+    @knowledge = []
+  end
+
+  def learn(str)
+    if !self.knowledge.include?(str)
+      @knowledge << str
+    end
+  end
 end
